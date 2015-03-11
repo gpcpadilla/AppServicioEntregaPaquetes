a24546f98d33f1f47855e4182789e7b39182af44
gpcpadilla <geovanito25@hotmail.com>
2015-03-11 16:45:39 -0500

    actualizacion

---
+++ AppServicioEntregaPaquete/Datos/Repositorios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AppServicioEntregaPaquete.Modelo
+
+namespace AppServisioEntregaPaquete.Datos
+{
+    public class Repositorios
+    {
+        private List<Package> data = new List<Package>();
+
+        public IEnumerable<Package> getPackage()
+        {
+            Package p1 = new Package();
+
+            Package p2 = new Package();
+
+            data.Add(p1);
+            data.Add(p2);
+
+            return data;
+        }
+        }
+    }
---
+++ AppServicioEntregaPaquete/Web/envio.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AppServicioEntregaPaquete.Modelo;
+using AppServicioEntregaPaquete.Datos;
+
+namespace AppServicioEntregaPaquete.Web
+{
+    public partial class envio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Repositorios lista = new Repositorios();
+
+            string result = "";
+
+            foreach (Package element in lista.getPackage())
+                result += "/n" + element;
+
+            Label1.Text = result.ToString().Replace("/n", "</br>");
+        }
+    }
+}
